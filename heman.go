1b41264bec58e076ef08720c755b30fdb1edbfe5
Mateusz Zawisza <mateusz.zawisza@gmail.com>
2014-07-10 00:28:18 +0200

    remove unneded stuff

--- heman.go
+++ heman.go
@@ -5,10 +5,5 @@ import "job"
 
 
 func main() {
-    //jobs_list := [][]string{
-    //    job.Jobs["tcp_dump"],
-    //    job.Jobs["connections_list"],
-    //    job.Jobs["connections_stats"],
-    //}
     executor.AsyncExecuteJobs(job.Jobs)
 }
