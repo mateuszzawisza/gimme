a9f786528b88e7a09b0b927fa6f049e6bb74d8cf
Mateusz Zawisza <mateusz.zawisza@gmail.com>
2014-07-10 01:00:48 +0200

    adds prepare

--- heman.go
+++ heman.go
@@ -1,9 +1,22 @@
 package main
 
-import "executor"
-import "jobs"
+import(
+    "fmt"
+    "executor"
+    "jobs"
+    "os"
+    "time"
+)
 
 
 func main() {
+    prepare()
     executor.AsyncExecuteJobs(jobs.Jobs)
+}
+
+func prepare(){
+    dir:="/root/heman/" + time.Now().Format("20060102150405")
+    os.MkdirAll(dir,os.ModeDir)
+    os.Chdir(dir)
+    fmt.Println("Saving stats into ",dir)
 }
