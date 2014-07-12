d3aa683c345640de5304e26b58fd69cfe1de6ccd
Mateusz Zawisza <mateusz.zawisza@gmail.com>
2014-07-13 00:55:38 +0200

    add flags instead of environment variables

--- gimme.go
+++ gimme.go
@@ -1,24 +1,109 @@
 package main
 
-import(
-    "fmt"
-    "executor"
-    "jobs"
-    "os"
-    "time"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/mateuszzawisza/gimme/executor"
+	"github.com/mateuszzawisza/gimme/jobs"
+
+	"launchpad.net/goamz/aws"
+	"launchpad.net/goamz/s3"
 )
 
-
-func main() {
-    prepare()
-    executor.AsyncExecuteJobs(jobs.Jobs)
+type Options struct {
+	AwsBucket, AwsAccessKeyId, AwsSecretAccessKey string
 }
 
-func prepare(){
-    dir:=os.Getenv("HOME") + "/gimme_output/" + time.Now().Format("20060102150405")
-    err:=os.MkdirAll(dir,os.ModeDir)
-    if err !=nil {panic(err)}
-    err=os.Chdir(dir)
-    if err !=nil {panic(err)}
-    fmt.Println("Saving stats into ",dir)
+func main() {
+	options := gather_flags()
+	path := prepare()
+	dir, file_name := dir_and_file_name(path)
+	executor.AsyncExecuteJobs(jobs.Jobs)
+	compressed_file_path := compress(dir, file_name)
+	s3Uploader(
+		options.AwsAccessKeyId,
+		options.AwsSecretAccessKey,
+		options.AwsBucket,
+		compressed_file_path,
+	)
+}
+
+func s3Uploader(aws_access_key_id, aws_secret_access_key, aws_bucket, file_path string) {
+	file, _ := os.Open(file_path)
+	file_stat, _ := file.Stat()
+	file_size := file_stat.Size()
+	content := make([]byte, file_size)
+	file.Read(content)
+
+	auth := aws.Auth{
+		AccessKey: aws_access_key_id,
+		SecretKey: aws_secret_access_key,
+	}
+	useast := aws.USEast
+	bucket_name := aws_bucket
+
+	connection := s3.New(auth, useast)
+	diag_bucket := connection.Bucket(bucket_name)
+	hostname, _ := os.Hostname()
+	_, file_name := dir_and_file_name(file_path)
+	s3_path := fmt.Sprintf("%s/%s", hostname, file_name)
+	fmt.Printf("Uploading file to https://s3.amazonaws.com/%s/%s\n", bucket_name, s3_path)
+	err := diag_bucket.Put(s3_path, content, "application/x-compressed", s3.BucketOwnerFull)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Done")
+}
+
+func prepare() string {
+	path := os.Getenv("HOME") + "/gimme_output/" + time.Now().Format("20060102150405")
+	err := os.MkdirAll(path, os.ModeDir)
+	if err != nil {
+		panic(err)
+	}
+	err = os.Chdir(path)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Saving stats into ", path)
+	return path
+}
+
+func compress(dir, output_name string) string {
+	err := os.Chdir(dir)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Creating tgz archive:  %s/%s.tar.gz\n", dir, output_name)
+	tar_command := fmt.Sprintf("tar cvzf %s.tar.gz %s", output_name, output_name)
+	_, err = exec.Command("sh", "-c", tar_command).Output()
+	if err != nil {
+		panic(err)
+	}
+	return fmt.Sprintf("%s/%s.tar.gz", dir, output_name)
+}
+
+func dir_and_file_name(path string) (string, string) {
+	split := strings.Split(path, "/")
+	dir := strings.Join(split[0:len(split)-1], "/")
+	file_name := split[len(split)-1:][0]
+	return dir, file_name
+}
+func gather_flags() Options {
+	var aws_bucket = flag.String("aws-bucket", "my-bucket-name", "Set the bucket name for the diagnostic information to be uploaded to")
+	var aws_access_key_id = flag.String("aws-access-key-id", "ACCESS_KEY_ID", "Set the access key id for AWS API communication")
+	var aws_secret_access_key = flag.String("aws-secret-access-key", "SECRET_ACCESS_KEY_ID", "Set the secret access key for AWS API communication")
+	flag.Parse()
+	options := Options{
+		AwsBucket:          *aws_bucket,
+		AwsAccessKeyId:     *aws_access_key_id,
+		AwsSecretAccessKey: *aws_secret_access_key,
+	}
+	return options
 }
