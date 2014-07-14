a62d0da5c7c9f14f75c739a699384f95de0d7f59
Mateusz Zawisza <mateusz.zawisza@gmail.com>
2014-07-14 10:33:14 +0200

    fixes error handling

--- archivist/archivist.go
+++ archivist/archivist.go
@@ -14,7 +14,10 @@ import (
 const s3UploadPartSize int64 = 5 * 1048576
 
 func S3Upload(aws_access_key_id, aws_secret_access_key, aws_bucket, file_path string) {
-	file, _ := os.Open(file_path)
+	file, err := os.Open(file_path)
+	if err != nil {
+		panic(err)
+	}
 	defer file.Close()
 	auth := aws.Auth{
 		AccessKey: aws_access_key_id,
@@ -28,29 +31,32 @@ func S3Upload(aws_access_key_id, aws_secret_access_key, aws_bucket, file_path st
 	_, file_name := dir_and_file_name(file_path)
 	s3_path := fmt.Sprintf("%s/%s", hostname, file_name)
 	log.Printf("Uploading file to https://s3.amazonaws.com/%s/%s\n", bucket_name, s3_path)
-	multi, err := diag_bucket.InitMulti(s3_path, "application/x-compressed", s3.BucketOwnerFull)
+	multi, multi_init_err := diag_bucket.InitMulti(s3_path, "application/x-compressed", s3.BucketOwnerFull)
+	if multi_init_err != nil {
+		log.Panic("Failed to upload file", multi_init_err)
+	}
 	parts, multi_err := multi.PutAll(file, s3UploadPartSize)
 	if multi_err != nil {
-		log.Panic("Failed to upload file", err)
+		log.Panic("Failed to upload file", multi_err)
 	}
 	complete_err := multi.Complete(parts)
 	if complete_err != nil {
-		log.Panic("Failed to upload file", err)
+		log.Panic("Failed to upload file", complete_err)
 	}
 	log.Println("Done")
 }
 
 func Compress(path string) string {
 	dir, archive_name := dir_and_file_name(path)
-	err := os.Chdir(dir)
-	if err != nil {
-		log.Panicf("Failed to change dir to output directory: %s\n", dir, err)
+	chdir_err := os.Chdir(dir)
+	if chdir_err != nil {
+		log.Panicf("Failed to change dir to output directory: %s\n", dir, chdir_err)
 	}
 	log.Printf("Creating tgz archive: %s/%s.tar.gz\n", dir, archive_name)
 	tar_command := fmt.Sprintf("tar cvzf %s.tar.gz %s", archive_name, archive_name)
-	_, err = exec.Command("sh", "-c", tar_command).Output()
-	if err != nil {
-		log.Panicf("Failed to execute: %s\n", tar_command, err)
+	_, exec_err := exec.Command("sh", "-c", tar_command).Output()
+	if exec_err != nil {
+		log.Panicf("Failed to execute: %s\n", tar_command, exec_err)
 	}
 	return fmt.Sprintf("%s/%s.tar.gz", dir, archive_name)
 }
